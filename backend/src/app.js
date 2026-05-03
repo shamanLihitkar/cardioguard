0d1305730f6ed1b54442ba0b7cf60b5e5b9d0910
Ibrahim <ikapadwanchwala@gmail.com>
2026-05-03 17:26:55 +0530

    Frontend deployed and cors issues handled

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,15 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://cardio-guard-two.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded());
 
