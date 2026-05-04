c8409a2871078df52892719977eca37deb0284cf
Ibrahim <ikapadwanchwala@gmail.com>
2026-05-04 13:15:37 +0530

    final testing 1000 users

--- vitals-test.js
+++ vitals-test.js
@@ -3,10 +3,10 @@ import { sleep } from "k6";
 
 export const options = {
   stages: [
-     { duration: "30s", target: 50 },
-    { duration: "1m", target: 150 },
+    { duration: "30s", target: 100 },
     { duration: "1m", target: 300 },
-    { duration: "1m", target: 400 },
+    { duration: "1m", target: 600 },
+    { duration: "1m", target: 1000 },
     { duration: "30s", target: 0 },
   ],
 };
